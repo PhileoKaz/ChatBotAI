8fb0afb4527d5120a14feb5c9177d7b23ec27de5
PhileoKaz <kazavangaphileo@gmail.com>
2018-04-12 21:55:05 +0200

    First commit

---
+++ app/src/main/java/edmt/dev/androidsimsimi/Models/ChatModel.java
@@ -0,0 +1,43 @@
+package edmt.dev.androidsimsimi.Models;
+
+/**
+ * Created by reale on 2/28/2017.
+ */
+
+public class ChatModel {
+    public String message;
+    public boolean isSend;
+
+    public ChatModel(String message, boolean isSend) {
+        this.message = message;
+        this.isSend = isSend;
+    }
+
+    public ChatModel() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setSend(boolean send) {
+        isSend = send;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ChatModel{" +
+                "message='" + message + '\'' +
+                ", isSend=" + isSend +
+                '}';
+    }
+}
